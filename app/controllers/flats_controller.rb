6fe0e7970eb91bb09f5dc5b8fe72cf565dd0dab5
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-04-22 22:22:14 -0300

    tried and failed to create search

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,4 +3,14 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def search
+    @parameter = params[:search].downcase
+    @flats = Flat.all.where("lower(name) LIKE :search", search: "#{@parameter}")
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :price_per_night, :number_of_guests)
+  end
 end
